29259a614631c97c174b894d86f4eaeb5c27eeaa
kantynium <Vladyslav.Savluk@lnu.edu.ua>
2025-04-28 14:01:49 +0300

    Lab1
    Lab1

---
+++ oop/ConsoleApplication2.cpp
@@ -0,0 +1,53 @@
+﻿#include "Price.h"
+
+void add(tovar &p1, tovar &p2){
+    p1.grn += p2.grn;
+    p1.cop += p2.cop;
+}
+void multiply(tovar &p1, int qual) {
+    p1.grn *= qual;
+    p1.cop *= qual;
+}
+void round(tovar &p1) {
+    if (p1.cop >= 100){
+        p1.grn = p1.grn + (p1.cop / 100);
+        p1.cop = p1.cop % 100;
+    }
+    if (p1.cop % 10 > 5) {
+        p1.cop += (10 - (p1.cop % 10));
+    } else if( p1.cop % 10 < 5) {
+        p1.cop -= p1.cop % 10;
+    }
+}
+
+void print(){ 
+    // Відкриття та читання файлу
+    ifstream file("lab1.txt");
+    if (!file) {
+        cout << "Помилка відкриття файлу!" << endl;
+        return;
+    }
+
+    //string name;
+    int grn, quantity;
+    short int kop;
+    tovar total = { 0, 0};
+
+    while (file >> grn >> kop >> quantity) {
+    // вивів значення з файлу
+    cout << grn << " грн. " << kop << " коп. " << quantity << " шт." << endl;
+    
+    tovar temp;
+    temp.grn = grn;
+    temp.cop = kop;
+
+    multiply(temp, quantity);
+    add(total, temp);
+    }
+    file.close();
+    // вивів загальну суму
+    cout << "Сума до оплати до заокруглення: " << total.grn << " грн. " << total.cop << " коп." << endl;
+    
+    round(total);
+    cout << "Сума всіх товарів після округлення: " << total.grn << " грн. " << total.cop << " коп." << endl;
+}
